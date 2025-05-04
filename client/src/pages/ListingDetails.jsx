3ebcdca3468a3452c428e86e514cf1f269606b42
Anubhav Kumar <118300550+AnubhavKumar1133@users.noreply.github.com>
2025-05-05 00:54:47 +0530

    Update ListingDetails.jsx

--- client/src/pages/ListingDetails.jsx
+++ client/src/pages/ListingDetails.jsx
@@ -20,7 +20,7 @@ const ListingDetails = () => {
   const getListingDetails = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/properties/${listingId}`,
+        BASE_URL+`/properties/${listingId}`,
         {
           method: "GET",
         }
